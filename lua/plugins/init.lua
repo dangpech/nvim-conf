2a05b277e371e1bd302d082a6aae763b88a82141
Dang Hai <pecheule34713@gmail.com>
2024-10-26 01:34:25 +0700

    update

--- lua/plugins/init.lua
+++ lua/plugins/init.lua
@@ -17,14 +17,57 @@ return {
   	"nvim-treesitter/nvim-treesitter",
   	opts = {
   		ensure_installed = {
-  			"vim", "lua", "vimdoc",
-       "html", "css",
-       "c", "bash", "cmake", "cpp", "csv",
-       "dockerfile", "git_config", "git_rebase",
-       "go", "gomod", "gosum", "gotmpl", "gowork",
-       "java", "javascript", "json5", "make", "rust",
-       "sql", "typescript", "xml", "yaml"
+        "vim", "lua", "vimdoc",
+        "html", "css",
+        "c", "bash", "cmake", "cpp", "csv",
+        "dockerfile", "git_config", "git_rebase",
+        "go", "gomod", "gosum", "gotmpl", "gowork",
+        "java", "javascript", "json5", "make", "rust",
+        "sql", "typescript", "xml", "yaml"
   		},
   	},
   },
+
+  {
+    "lewis6991/gitsigns.nvim",
+    config = function()
+        require("gitsigns").setup()
+    end
+  },
+
+  {
+    "lukas-reineke/indent-blankline.nvim",
+    main = "ibl",
+    ---@module "ibl"
+    ---@type ibl.config
+    opts = {},
+    config = function ()
+      require("ibl").setup()
+    end
+  }
+
+  -- {
+  --   "nvim-tree/nvim-tree.lua",
+  --   version = "",
+  --   lazy = false,
+  --   dependencies = {
+  --     "nvim-tree/nvim-web-devicons",
+  --   },
+  --   config = function ()
+  --     require("nvim-tree").setup {
+  --       sort = {
+  --         sorter = "case_sensitive",
+  --       },
+  --       view = {
+  --         width = 30,
+  --       },
+  --       renderer = {
+  --         group_empty = true,
+  --       },
+  --       filters = {
+  --         dotfiles = true,
+  --       },
+  --     }
+  --   end
+  -- }
 }
